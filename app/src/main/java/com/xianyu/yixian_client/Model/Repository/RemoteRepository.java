a3ed3698621ff63803dfb849a24e17c1cd270c08
槿墨丶 潇仙 <839336369@qq.com>
2021-01-15 18:58:37 +0800

    新一波优化

--- app/src/main/java/com/xianyu/yixian_client/Model/Repository/RemoteRepository.java
+++ app/src/main/java/com/xianyu/yixian_client/Model/Repository/RemoteRepository.java
@@ -23,7 +23,7 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class RemoteRepository {
     public RemoteRepository(){
-        boolean remote = true;
+        boolean remote = false;
         if(remote){
             IUser user = RPCRequestProxyFactory.Register(IUser.class,"User","192.168.0.105","28015");
             Observable.create((ObservableOnSubscribe<Void>) emitter -> user.hello("你好")).subscribeOn(Schedulers.io()).subscribe();
